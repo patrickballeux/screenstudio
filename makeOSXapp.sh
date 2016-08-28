25da16cae3045a4e354f6016a4f5e7017e536038
patrickballeux <patrickballeux@192.168.0.118>
2016-08-28 08:45:30 -0400

    Added new icon for OSX

--- makeOSXapp.sh
+++ makeOSXapp.sh
@@ -5,23 +5,20 @@ sed "s/MAIN = \".*\"/MAIN = \"$VERSION\"/g" src/screenstudio/Version.java>src/sc
 rm src/screenstudio/Version.java
 mv src/screenstudio/Version.java.temp src/screenstudio/Version.java
 ant -Dnb.internal.action.name=rebuild clean
-rm Capture/*.*
-tar -zcvf "../ScreenStudio-OSX-$VERSION-src.tar.gz" .
 ant -Dnb.internal.action.name=jar
 echo "Building OSX app"
 echo "Removing previous build..."
 echo "Creating new folder app..."
 cp -r apps/OSX/ ./ScreenStudio.app
-mkdir ScreenStudio.app/Contents/MacOS/RTMP
 mkdir ScreenStudio.app/Contents/MacOS/FFMPEG
 mkdir ScreenStudio.app/Contents/MacOS/lib
 echo "Copying ScreenStudio archive..."
 cp dist/ScreenStudio.jar ScreenStudio.app/Contents/MacOS/ScreenStudio.jar
 echo "Copying  files..."
 cp apps/README.txt ScreenStudio.app/Contents/MacOS/README.txt
-cp RTMP/*.properties ScreenStudio.app/Contents/MacOS/RTMP
-cp FFMPEG/osx.properties ScreenStudio.app/Contents/MacOS/FFMPEG
 cp FFMPEG/ffmpeg-osx ScreenStudio.app/Contents/MacOS/FFMPEG
+chmod +x ScreenStudio.app/Contents/MacOS/FFMPEG/ffmpeg-osx
+chmod +x ScreenStudio.app/Contents/MacOS/launcher
 cp libs/* ScreenStudio.app/Contents/MacOS/lib
 tar -zcvf "../ScreenStudio-OSX-$VERSION-bin.tar.gz" ScreenStudio.app
 echo "$VERSION">../osx.last.version
