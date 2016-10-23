94049d61c3e90261f0607292dd6c999a2d394fd5
patri <patri@192.168.0.104>
2016-10-23 14:44:12 -0400

    Moving to next version - Added screen area capture

--- src/screenstudio/Version.java
+++ src/screenstudio/Version.java
@@ -27,7 +27,7 @@ import screenstudio.sources.Screen;
 public class Version {
 //Using this to set the main version of ScreenStudio
 
-    public final static String MAIN = "3.0.8";
+    public final static String MAIN = "3.0.9";
 
     public static boolean hasNewVersion() {
         boolean retValue = false;
