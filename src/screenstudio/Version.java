61395f5c1c6ff5cad32253d7dc48e8c032bcb77b
patrick <patrick@patrick-MacBook>
2017-01-15 09:37:02 -0500

    Moving to version 3.1.0

--- src/screenstudio/Version.java
+++ src/screenstudio/Version.java
@@ -27,7 +27,7 @@ import screenstudio.sources.Screen;
 public class Version {
 //Using this to set the main version of ScreenStudio
 
-    public final static String MAIN = "3.0.12";
+    public final static String MAIN = "3.1.0";
 
     public static boolean hasNewVersion() {
         boolean retValue = false;
